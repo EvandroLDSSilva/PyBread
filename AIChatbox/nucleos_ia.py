d95ad1c7403e9b2a2e48bc8d2e39e6357eedbf2f
Acis Renan <acisinn22@gmail.com>
2025-05-25 14:09:09 -0300

    Chatbox IA criado

--- AIChatbox/nucleos_ia.py
+++ AIChatbox/nucleos_ia.py
@@ -1,14 +1,29 @@
 import os
 from langchain_groq import ChatGroq
 from langchain.prompts import ChatPromptTemplate
-#API KEY
-api_key = "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"
 
+api_key =  "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"
+os.environ['GROQ_API_KEY'] = api_key
 
-os.environ["GROQ_API_KEY"] = api_key
+chat = ChatGroq(model='llama-3.3-70b-versatile')
 
+def resposta_bot(mensagens):
+  mensagens_modelo = [('system', 'Você é um assistente amigável chamado Baguette oIa')]
+  mensagens_modelo += mensagens
+  template = ChatPromptTemplate.from_messages(mensagens_modelo)
+  chain = template | chat
+  return chain.invoke({}).content
 
-chat = ChatGroq(model ='llama-3.3-70b-versatile')
+print('Bem-vindo ao Baguette Ia')
 
-primeira_interacao = chat.invoke('qual o melhor sistema de vendas do mundo?')
-print (primeira_interacao.content)
+mensagens = []
+while True:
+  pergunta = input('Usuario: ')
+  if pergunta.lower() == 'x' or pergunta.lower() == 'sair'  :
+    break
+  mensagens.append(('user', pergunta))
+  resposta = resposta_bot(mensagens)
+  mensagens.append(('assistant', resposta))
+  print(f'Bot: {resposta}')
+
+print('Muito obrigado por usar o Baguette IA')
